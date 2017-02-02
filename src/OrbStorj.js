c085022766a7c56b1e84b08bc326502b028cc920
Aitrean <williamprado@cmail.carleton.ca>
2017-02-02 14:56:51 -0800

    Implemented a very basic model of an opening menu for taking optional arguments.

--- src/OrbStorj.js
+++ src/OrbStorj.js
@@ -1,12 +1,19 @@
 'use strict'
 
 const orbitdb = require('./lib/orbitHandler');
+const menu = require('./lib/consoleMenu');
 
-async function start(){
-  await orbitdb.init('myApp');
-  let myDb = await orbitdb.mkDB(['begin', 'here'], 'kvstore');
-  
-  await myDb.put('hih','poopy')
-  console.log(myDb.get('hih'));
+async function start() {
+	await orbitdb.init('myApp');
+	//let myDb = await orbitdb.mkDB(['begin', 'here'], 'kvstore');
+
+	let action = await menu.openingMenu();
+	switch (action.type) {
+		case ("launchDefaultDatabaseConnection"):
+			let myDb = await orbitdb.mkDB([action.data, 'type'], 'kvstore');
+			break;
+		default:
+			break;
+	}
 };
 start();
--- src/lib/consoleMenu.js
+++ src/lib/consoleMenu.js
@@ -1,9 +1,44 @@
 const cli = require('clui');
 const inquirer = require('inquirer');
+const minimist = require('minimist');
+const package = require('../../package.json');
 
 const openingMenu = () => {
-	console.log("OrbStorj");
+	let argv = minimist(process.argv.slice(2), {
+		boolean: false
+	});
+	return new Promise((resolve, reject) => {
+		if (argv.version) {
+			console.log("OrbStorj version " + package.version);
+			resolve();
+		} else if (argv.help) {
+			console.log("Commands:");
+			console.log("--version: Displays version of OrbStorj");
+			resolve();
+		} else if (argv.launch) {
+			console.log("Launching...");
+			resolve(launchMenu(argv));
+		}
+	});
 }
+
+//TODO: implement menu for user input
+
+const launchMenu = (argv) => {
+	if (argv) {
+		return ({
+			type: "launchDatabaseConnection",
+			data: {
+				argv
+			}
+		});
+	} else {
+		return ({
+			type: "launchDefaultDatabaseConnection",
+			data: {}
+		});
+	};
+};
 
 const exportObj = {
 	openingMenu: openingMenu
