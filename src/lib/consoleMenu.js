093f85045d842768997f9154ea6b72c218ea3880
Ubuntu <williamprado@cmail.carleton.ca>
2017-02-02 10:34:00 -0800

    Small code changes, todo list

---
+++ src/lib/consoleMenu.js
@@ -0,0 +1,12 @@
+const cli = require('clui');
+const inquirer = require('inquirer');
+
+const openingMenu = () => {
+	console.log("OrbStorj");
+}
+
+const exportObj = {
+	openingMenu: openingMenu
+}
+
+module.exports = exportObj;
