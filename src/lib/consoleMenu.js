936d33d1eaa2eeda57fee60e07cbb93b0c8df7a9
aitrean <williamprado@cmail.carleton.ca>
2017-02-21 21:37:53 -0500

    Added functionality to support user input

--- src/lib/consoleMenu.js
+++ src/lib/consoleMenu.js
@@ -1,8 +1,10 @@
-const cli = require('clui');
-const inquirer = require('inquirer');
 const minimist = require('minimist');
 const package = require('../../package.json');
 
+/**
+ * Opening menu interprets CLI arguments, parses, then sends back to controller in OrbStorj
+ * @return Promise
+ */
 const openingMenu = () => {
 	let argv = minimist(process.argv.slice(2), {
 		boolean: false
@@ -13,11 +15,15 @@ const openingMenu = () => {
 			resolve();
 		} else if (argv.help) {
 			console.log('Commands:');
-			console.log('--version: Displays version of OrbStorj');
+			console.log('version		Displays version of OrbStorj');
+			console.log('help		Displays list of commands that operate with OrbStorj');
+			console.log('launch		launch into the given database, if no database given, launch default');
 			resolve();
 		} else if (argv.launch) {
 			console.log('Launching...');
 			resolve(launchMenu(argv));
+		} else {
+			reject('Could not read that command, see --help for more details');
 		}
 	});
 };
@@ -25,17 +31,15 @@ const openingMenu = () => {
 //TODO: implement menu for user input
 
 const launchMenu = (argv) => {
-	if (argv) {
-		return ({
-			type: 'launchDatabaseConnection',
-			data: {
-				argv
-			}
-		});
-	} else {
+	if (argv.launch === true) {
 		return ({
 			type: 'launchDefaultDatabaseConnection',
 			data: {}
+		});
+	} else {
+		return ({
+			type: 'launchDatabaseConnection',
+			data: argv.launch
 		});
 	}
 };
@@ -45,3 +49,4 @@ const exportObj = {
 };
 
 module.exports = exportObj;
+
